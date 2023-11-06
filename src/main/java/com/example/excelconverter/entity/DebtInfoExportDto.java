19a47c455bba43b8c4dd6bc05ea483af93759e36
fengchengtang-c <fengchengtang-c@deloitte.com.cn>
2023-11-06 17:17:48 +0800

    first commit2

---
+++ src/main/java/com/example/excelconverter/entity/DebtInfoExportDto.java
@@ -0,0 +1,13 @@
+package com.example.excelconverter.entity;
+
+import lombok.Data;
+
+/**
+ * @author Frank.Tang
+ * @date 2023-11-06 17:14
+ * @desc
+ **/
+@Data
+public class DebtInfoExportDto {
+
+}
